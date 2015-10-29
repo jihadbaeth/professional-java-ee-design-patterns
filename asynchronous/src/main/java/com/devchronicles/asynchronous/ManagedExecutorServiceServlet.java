e23bf4c7c0b7ca5258b919d9055271103ca94749
emag <lanabe.lanabe@gmail.com>
2015-10-30 00:49:18 +0900

    Add a servlet using ManagedExecutorService. but it's broken. WildFly Swarm can't lookup it

---
+++ asynchronous/src/main/java/com/devchronicles/asynchronous/ManagedExecutorServiceServlet.java
@@ -0,0 +1,34 @@
+package com.devchronicles.asynchronous;
+
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Yoshimasa Tanabe
+ */
+@WebServlet(urlPatterns = "/managed-executor-service", asyncSupported = true)
+public class ManagedExecutorServiceServlet extends HttpServlet {
+
+//  @Resource
+  private ManagedExecutorService executor;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    AsyncContext asyncContext = req.startAsync();
+    PrintWriter writer = resp.getWriter();
+
+    executor.submit(() -> {
+      writer.println("Complete");
+      asyncContext.complete();
+    });
+  }
+
+}
